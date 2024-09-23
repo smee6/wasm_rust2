598945a453538d1a17949ae61c330faa48c25d3d
seomyungin <smee6@naver.com>
2024-09-23 14:22:20 +0900

    build sh mod

--- rust-hyper/build.sh
+++ rust-hyper/build.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# 루트 디렉토리 경로 설정
+# root directory of this script
 ROOT_DIR=$(pwd)
 
-# 모듈 디렉토리 경로 설정
+# module directory
 MODULES_DIR="$ROOT_DIR/modules"
 
-# 생성된 .wasm 파일을 복사할 경로 설정 (예: root 디렉토리의 wasm 폴더)
+# set the target directory to copy the generated .wasm files (e.g., wasm folder in the root directory)
 TARGET_DIR="$ROOT_DIR"
 
-# 복사할 디렉토리가 없다면 생성
+# when the target directory does not exist, create it
 mkdir -p "$TARGET_DIR"
 
-# modules 디렉토리 내의 각 서브 폴더를 순회
+# visit each subfolder in the modules directory
 for dir in "$MODULES_DIR"/*/; do
     if [ -d "$dir" ]; then
-        # 서브 폴더로 이동
+        # change directory to the module directory
         echo "Processing directory: $dir"
         cd "$dir" || { echo "Failed to change directory to $dir"; exit 1; }
 
-        # 서브 폴더 내에서 cargo build 실행 (wasm 타겟)
+        # run cargo build in the subfolder (wasm target)
         echo "Building module in directory: $dir"
         cargo build --target wasm32-unknown-unknown --release
 
-        # 빌드가 성공했는지 확인
+        # check if the build is successful or not
         if [ $? -eq 0 ]; then
-            # 릴리즈 폴더에서 wasm 파일 복사
+            # copy the wasm file to the target directory
             wasm_dir="$dir/target/wasm32-unknown-unknown/release"
             echo "Searching for wasm files in $wasm_dir..."
             ls "$wasm_dir"
 
-            # find 명령어로 wasm 파일 찾기
+            # search for the wasm file in the release directory
             wasm_file=$(find "$wasm_dir" -name "*.wasm" | head -n 1)
 
             if [ -n "$wasm_file" ]; then
@@ -44,11 +44,14 @@ for dir in "$MODULES_DIR"/*/; do
             echo "Build failed in $dir"
         fi
 
-        # 루트 디렉토리로 돌아감
+        # back to the root directory
         cd "$ROOT_DIR" || { echo "Failed to change directory to $ROOT_DIR"; exit 1; }
     fi
 done
 
+# log the completion of the build and copy process
 echo "Build and copy process completed."
 echo "Soon Cargo will run the server."
+
+# run the server
 cargo run --release
